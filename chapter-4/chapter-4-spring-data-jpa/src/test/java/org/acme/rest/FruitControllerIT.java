6af57df2315d722ef821646453a8251812875c1e
Eric Deandrea <eric.deandrea@gmail.com>
2022-07-22 14:28:14 -0400

    Step 3

--- chapter-4/chapter-4-spring-data-jpa/src/test/java/org/acme/rest/FruitControllerIT.java
+++ chapter-4/chapter-4-spring-data-jpa/src/test/java/org/acme/rest/FruitControllerIT.java
@@ -20,26 +20,29 @@ public class FruitControllerIT {
 	private static final int DEFAULT_ORDER = 1;
 
 	@Test
-	@Order(DEFAULT_ORDER + 2)
+	@Order(DEFAULT_ORDER)
 	public void getAll() {
 		get("/fruits").then()
 			.statusCode(Status.OK.getStatusCode())
 			.contentType(ContentType.JSON)
-			.body("$.size()", is(1))
+			.body("$.size()", is(2))
 			.body("[0].id", greaterThanOrEqualTo(1))
-			.body("[0].name", is("Grapefruit"))
-			.body("[0].description", is("Summer fruit"));
+			.body("[0].name", is("Apple"))
+			.body("[0].description", is("Hearty fruit"))
+			.body("[1].id", greaterThanOrEqualTo(1))
+			.body("[1].name", is("Pear"))
+			.body("[1].description", is("Juicy fruit"));
 	}
 
 	@Test
-	@Order(DEFAULT_ORDER + 2)
+	@Order(DEFAULT_ORDER)
 	public void getFruitFound() {
-		get("/fruits/Grapefruit").then()
+		get("/fruits/Apple").then()
 			.statusCode(Status.OK.getStatusCode())
 			.contentType(ContentType.JSON)
 			.body("id", greaterThanOrEqualTo(1))
-			.body("name", is("Grapefruit"))
-			.body("description", is("Summer fruit"));
+			.body("name", is("Apple"))
+			.body("description", is("Hearty fruit"));
 	}
 
 	@Test
@@ -53,7 +56,7 @@ public class FruitControllerIT {
 	@Order(DEFAULT_ORDER + 1)
 	public void addFruit() {
 		get("/fruits").then()
-			.body("$.size()", is(0));
+			.body("$.size()", is(2));
 
 		given()
 			.contentType(ContentType.JSON)
@@ -62,11 +65,11 @@ public class FruitControllerIT {
 			.then()
 			.contentType(ContentType.JSON)
 			.statusCode(Status.OK.getStatusCode())
-			.body("id", greaterThanOrEqualTo(1))
+			.body("id", greaterThanOrEqualTo(3))
 			.body("name", is("Grapefruit"))
 			.body("description", is("Summer fruit"));
 
 		get("/fruits").then()
-			.body("$.size()", is(1));
+			.body("$.size()", is(3));
 	}
 }
